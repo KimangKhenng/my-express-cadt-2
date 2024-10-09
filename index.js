dea162eb76c5097454bba97acd7f1ec51d12dff2
tfd-ed <info@tfdevs.com>
2024-10-09 10:52:43 +0700

    Middleware

--- index.js
+++ index.js
@@ -2,22 +2,43 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+/**
+ * Authenticate
+ * Authorize
+ * Error Checking
+ */
+function logger(req, res, next) {
+    // console.log(req)
+    console.log("Incoming request", req.rawHeaders[3])
+    // Example, request from unauthorized user
+    // return res.status(404).send("Forbidden")
+    next()
+}
+
+function authroize(req, res, next) {
+    return res.status(404).json({
+        msg: "Not Found"
+    })
+}
+
 // app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
+app.use(logger)
+
 
 app.get('/courses', function (req, res) {
-    res.status(202).send("Hello")
+    return res.status(202).send("Hello")
 })
 
 // Allow user to create courses
-app.post('/courses', (req, res) => {
+app.post('/courses', authroize, (req, res) => {
     console.log(req.body)
-    res.json(req.body)
+    return res.json(req.body)
 })
 
 // Conventional function
 app.get('/', function (req, res) {
-    res.json({
+    return res.json({
         key: 'sdsdfsdf',
         id: 'sdsfdsdf'
     })
@@ -25,7 +46,7 @@ app.get('/', function (req, res) {
 
 // Arrow function
 app.get('/shortcut', (req, res) => {
-    res.json({
+    return res.json({
         message: "Hello World"
     })
 })
