c9d11a36247d8a692788772c3852964489aebfc5
tfd-ed <info@tfdevs.com>
2024-10-24 09:57:14 +0700

    Save File DB

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -113,7 +113,26 @@ const singleUpload = multer({
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb)
+    },
 }).single('file')
+
+// Check file type
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif|pdf/
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype)
+
+    if (mimetype && extname) {
+        return cb(null, true)
+    } else {
+        cb(new Error('Error: Images Only!'), false)
+    }
+}
 
 
 module.exports = {
